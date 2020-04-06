ec1c90e6fbdab5038c5616ea24ab3ef32e35842f
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-04-05 22:37:55 -0500

    Loaded the project and have Input being taken in

---
+++ original/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+#include "Containers/UnrealString.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Hello and welcome to Bull Cow!!"));
+    PrintLine(TEXT("By pressing tab, then enter you will be able to type on the console"));
+    PrintLine(TEXT("What is your name?\n"));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+    FString HiddenWord = TEXT("ride");
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You Win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("YOU SUCK!"));
+    }
+    
+}
